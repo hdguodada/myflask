9825f4c58a6a91695721d8195209a09f82bc9769
yumo <261731963@qq.com>
2017-08-25 15:02:56 +0800

    myflask

--- example1.py
+++ example1.py
@@ -1,10 +1,12 @@
-from flask import Flask, url_for
-from flask import render_template
+from flask import Flask, url_for, flash
+from flask import render_template, session, redirect
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 
@@ -12,19 +14,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'NIDAYE'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:passwd@loaclhost/myflask'
+# 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它。
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
-# 动态路由 + 请求上下文
+# 动态路由 + 请求上下文 + 重定向
 @app.route('/user/<name>')
 def user(name):
     print(url_for('user', name=name, _external=True))
@@ -41,12 +50,34 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('what is your name ?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
+# 定义模型
+class Role(db.Model):
+    __tablename__ = 'roles' # 定义数据库中使用到表名
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r' % self.username
+
+
+
+    
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
 
 
