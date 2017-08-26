745a401054af0bafade653c4eaadc7d8e537c149
yumo <261731963@qq.com>
2017-08-26 10:23:26 +0800

    ddd

--- example1.py
+++ example1.py
@@ -17,7 +17,7 @@ bootstrap = Bootstrap(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:passwd@localhost/myflask'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True  # 这一项是每次请求结束后都会自动提交数据库中的变动
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它。
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 如果设置成 True (默认情况)，Flask-SQLAlchemy 将会追踪对象的修改并且发送信号。这需要额外的内存， 如果不必要的可以禁用它。
 db = SQLAlchemy(app)
 
 
@@ -25,12 +25,18 @@ db = SQLAlchemy(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 # 动态路由 + 请求上下文 + 重定向
@@ -60,7 +66,8 @@ class Role(db.Model):
     __tablename__ = 'roles'  # 定义数据库中使用到表名
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-
+    users = db.relationship('User', backref='role')
+    
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -69,7 +76,8 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
     def __repr__(self):
         return '<User %r' % self.username
 
@@ -78,6 +86,6 @@ class User(db.Model):
     
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
 
 
