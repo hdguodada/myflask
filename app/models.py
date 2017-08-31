97b5955a7a3ac15cbc9616c23b9eb44062b52440
guojing <261731963@qq.com>
2017-08-31 22:10:43 +0800

    831

--- app/models.py
+++ app/models.py
@@ -1,5 +1,13 @@
 # 定义模型
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Role(db.Model):
@@ -12,12 +20,28 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     userid = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    
+    @property # 讲password方法设置为只读属性，可以用User.password
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    
+    @password.setter # 只读属性的设置方法
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        ''
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return '<User %r' % self.username
--- config.py
+++ config.py
@@ -6,9 +6,10 @@ class Config:
     '基类，定义基本配置'
     SECRET_KEY = 'hard to guess'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASY]'
-    FLASKY_MAIL_SENDER = '18358928574@sina.cn'
-    FLASKY_ADMIN = '18358928574@sina.cn'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[FLASY]' # 主题前面加的修饰前缀
+    FLASKY_MAIL_SENDER = 'FLASKY Admin <18358928574@sina.cn>' # <> 前面的是昵称， <>里的是发件人
+    FLASKY_ADMIN = '18358928574@sina.cn' # 指管理员是谁
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
@@ -21,20 +22,20 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.sina.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
-    SQLALCHEMY_DATABASE_URI = ''
+    MAIL_USERNAME = '18358928574@sina.cn'
+    MAIL_PASSWORD = 'ajian2013'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:passwd@localhost/myflask'
 
 
 class TestingConfig(Config):
     '测试环境库'
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:passwd@localhost/myflask'
 
 
 class ProductConfig(Config):
     '生产环境库'
-    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:passwd@localhost/myflask'
 
 
 '注册不同的环境和一个默认配置'
